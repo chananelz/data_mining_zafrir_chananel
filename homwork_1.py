288f106affc77d123cc3b23ff0f9f2ab6a8ff92f
zchananel <chananelz@gmail.com>
2021-11-03 09:42:15 +0200

    step 2 'working form Chananel zaguri'

--- homwork_1.py
+++ homwork_1.py
@@ -1,16 +1,45 @@
-# This is a sample Python script.
+# homework 01
+# Chananel Zaguri 206275711
+# This function read k txt fill and creat k arff fill correspondingly
+# In the arff fill each line describe a patient and contain:
+# ID
+# Time
+# category low/high
+#הערה חשובה- המרצה אמר שלנפות זה להשים סימן שאלה ולא להתעלם לגמרי.
+from math import sqrt
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def convert2arff(k):
+    for i in range(1, k + 1):  # Go over each file
+        fout = open("temp" + str(i) + ".arff", "w")  # read fill
+        fout.write("@relation patients_temperatures\n")  # write the opening
+        fout.write("@attribute patients_ID numeric\n")
+        fout.write("@attribute time numeric\n")
+        fout.write("@attribute temperatue {Low,High} \n\n")
+        fout.write("@data\n")
+
+        fin = open(str(i) + ".txt", "r")  # open the txt fill
+        for time in range(60 * 12):  # slicing - we take into account only 12 hours
+            s = fin.readline().split()
+            for patient in range(len(s)):  # go over each patient
+                temp = float(s[patient])
+                roll_up = ""  # save the category
+                if temp < 36 or temp > 43:  # check if we need to convert fahrenheit -> celsius
+                    temp = (temp - 32) / 1.8  # convert according to the formula
+                if temp < 36 or temp > 43:
+                    temp = "?"  # if we have error in the data leave out this data
+                if temp == "?":
+                    continue
+                    # roll_up = "?"
+                else:
+                    if temp > 37:  # we want to classify the data to two section "High" and "Low"
+                        roll_up = "High"
+                    else:
+                        roll_up = "Low"
+
+                fout.write(str(patient + 1) + "," + str(time) + "," + roll_up + "\n")  # write the data in arff fill
+        fin.close()  # close the data
+    fout.close()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+convert2arff(3)
